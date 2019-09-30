bfe0a86f7a41cc2e682be9b7ed92d1830e414e12
Yuliia Kandych <yuliiakandych@YuliiaD13819992.fanshawec.ca>
2019-09-30 19:37:40 -0400

    30.09.2019

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path'); // path lets us navigate the firs system
+const hbs = require('hbs');
+
+const sql = require('./utils/sql');
+
 
 // heroky assigns a port it deploys via process (environment variables - coming)
 // locally this  run ..
@@ -7,20 +11,55 @@ const port = process.env.PORT || 3000; //a double pipe - ||-  means or
 
 const app = express();
 
-app.use (express.static('public'));
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 
 
 app.get ('/', (red, res) => {
-  console.log('at the home route')
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  console.log('at the home route');
+
+//res.sendFile(path.join(__dirname + '/views/index.html'));
+
+  res.render('home', {message: "hi there", anothermessage: "this is easy! haha" } );
+  
 })
 
 app.get('/contact', (req,res) => {
   console.log('at the cont route')
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+  // res.sendFile(path.join(__dirname + '/views/contact.html'));
+  res.render('contact', {message: "What is your name?"})
 
 })
+
+
+app.get('/users', (req,res) => {
+  console.log('at the users route')
+
+  sql.getConnection((err, connections) => {
+    // handle errors first
+    if (err) {
+      return console.log(err.message);
+    }
+
+    let query = `SELECT * FROM tbl_card`;
+
+    sql.query(query, (err, rows) => {
+      connections.release();
+
+      if (err) 
+      {return console.log(err.message);
+      }
+
+      console.log(rows);
+
+      res.render('user', rows[0]);
+    })
+  })
+})
+
 
 app.get('/portfolio', (req,res) => {
   console.log('at the port route')
@@ -30,4 +69,4 @@ app.get('/portfolio', (req,res) => {
 
 app.listen(port, () => {
   console.log(`Server running at ${port}/`);
-});
+})
---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    uname: 'root',
+    upass: 'root',
+    port: 8889,
+    host: 'localhost',
+    db: 'bizcard'
+}
+
+module.exports = config;
